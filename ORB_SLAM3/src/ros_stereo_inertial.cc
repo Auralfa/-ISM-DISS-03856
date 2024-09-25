95845e878d8bbe136c866df6d1a07aa44fce6140
Auralfa <146801628+Auralfa@users.noreply.github.com>
2024-09-25 13:17:17 +0800

    Update ros_stereo_inertial.cc

--- ORB_SLAM3/src/ros_stereo_inertial.cc
+++ ORB_SLAM3/src/ros_stereo_inertial.cc
@@ -31,12 +31,8 @@
 
 #include<opencv2/core/core.hpp>
 
-#include"../../../../include/System.h" //!zw
+#include"../../../../include/System.h" //!
 #include"../include/ImuTypes.h"
-
-#include <tf/transform_broadcaster.h> //!zw
-#include <nav_msgs/Odometry.h> //!zw
-
 
 using namespace std;
 
@@ -53,14 +49,12 @@ public:
 class ImageGrabber
 {
 public:
-    ImageGrabber(ORB_SLAM3::System* pSLAM, ImuGrabber *pImuGb, const bool bRect, const bool bClahe);
+    ImageGrabber(ORB_SLAM3::System* pSLAM, ImuGrabber *pImuGb, const bool bRect, const bool bClahe): mpSLAM(pSLAM), mpImuGb(pImuGb), do_rectify(bRect), mbClahe(bClahe){}
 
     void GrabImageLeft(const sensor_msgs::ImageConstPtr& msg);
     void GrabImageRight(const sensor_msgs::ImageConstPtr& msg);
     cv::Mat GetImage(const sensor_msgs::ImageConstPtr &img_msg);
     void SyncWithImu();
-
-    void PublishTransform(const Sophus::SE3f &Tcw, const ros::Time &stamp); // 添加函数声明
 
     queue<sensor_msgs::ImageConstPtr> imgLeftBuf, imgRightBuf;
     std::mutex mBufMutexLeft,mBufMutexRight;
@@ -73,32 +67,9 @@ public:
 
     const bool mbClahe;
     cv::Ptr<cv::CLAHE> mClahe = cv::createCLAHE(3.0, cv::Size(8, 8));
-    tf::TransformBroadcaster tfBroadcaster; // 添加tf广播成员
 };
 
 
-ImageGrabber::ImageGrabber(ORB_SLAM3::System* pSLAM, ImuGrabber* pImuGb, bool bRect, bool bClahe)
-    : mpSLAM(pSLAM), mpImuGb(pImuGb), do_rectify(bRect), mbClahe(bClahe)
-{
-    if(mbClahe)
-    {
-        mClahe = cv::createCLAHE(3.0, cv::Size(8, 8));
-    }
-}
-
-// void publishTF()
-// {
-//     static tf::TransformBroadcaster br;
-//     tf::Transform transform;
-//     transform.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
-    
-//     // 创建一个90度旋转矩阵，绕y轴旋转使z轴对齐x轴
-//     tf::Quaternion q;
-//     q.setRPY(0, -M_PI/2, 0); // 这里是绕y轴旋转90度
-//     transform.setRotation(q);
-
-//     br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "camera_link", "camera_corrected"));
-// }
 
 int main(int argc, char **argv)
 {
@@ -170,69 +141,10 @@ int main(int argc, char **argv)
   ros::Subscriber sub_imu = n.subscribe("/stereo_inertial_publisher/imu", 1000, &ImuGrabber::GrabImu, &imugb); 
   ros::Subscriber sub_img_left = n.subscribe("/stereo_inertial_publisher/left/image_rect", 100, &ImageGrabber::GrabImageLeft,&igb);
   ros::Subscriber sub_img_right = n.subscribe("/stereo_inertial_publisher/right/image_rect", 100, &ImageGrabber::GrabImageRight,&igb);
-  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("orb3_odom", 1000);
+
   std::thread sync_thread(&ImageGrabber::SyncWithImu,&igb);
 
-  // ros::Rate loop_rate(10000);  // 控制主循环频率
-
-  // while (ros::ok())
-  // {
-  //   //  发布变换
-  //   publishTF();  // 在这里调用 
-
-  //   ros::spinOnce();  // 处理回调函数
-  //   loop_rate.sleep();  // 控制循环频率
-  // }
-    tf::TransformBroadcaster odom_broadcaster;
-    Sophus::SE3f pose = SLAM.GetCurrentFrame(); 
-    auto currentFrame = SLAM.GetCurrentFrame();
-    Eigen::Matrix4f identity_matrix = Eigen::Matrix4f::Identity();
-    if (!pose.matrix().isApprox(identity_matrix)) 
-    {
-        // 发布tf变换
-        geometry_msgs::TransformStamped odom_trans;
-        odom_trans.header.stamp = ros::Time::now();
-        odom_trans.header.frame_id = "orb3_odom";
-        odom_trans.child_frame_id = "base_link";
-
-        odom_trans.transform.translation.x = pose.translation().x();
-        odom_trans.transform.translation.y = pose.translation().y();
-        odom_trans.transform.translation.z = pose.translation().z();
-        
-        Eigen::Quaternionf q(pose.unit_quaternion());
-        odom_trans.transform.rotation.x = q.x();
-        odom_trans.transform.rotation.y = q.y();
-        odom_trans.transform.rotation.z = q.z();
-        odom_trans.transform.rotation.w = q.w();
-
-        // 发布tf变换
-        odom_broadcaster.sendTransform(odom_trans);
-
-        // 同时发布odom消息
-        nav_msgs::Odometry odom_msg;
-        odom_msg.header.stamp = odom_trans.header.stamp;
-        odom_msg.header.frame_id = "orb3_odom";
-        odom_msg.child_frame_id = "base_link";
-
-        odom_msg.pose.pose.position.x = pose.translation().x();
-        odom_msg.pose.pose.position.y = pose.translation().y();
-        odom_msg.pose.pose.position.z = pose.translation().z();
-        odom_msg.pose.pose.orientation = odom_trans.transform.rotation;
-
-        odom_pub.publish(odom_msg);
-    }
-
-
   ros::spin();
-
-  // Stop all threads
-  SLAM.Shutdown();
-
-  // Save camera trajectory
-  SLAM.SaveKeyFrameTrajectoryTUM("KeyFrame_Stereo_Inertial.txt");
-  SLAM.SaveTrajectoryTUM("Trajectory_Stereo_Inertial.txt");
-
-  ros::shutdown();
 
   return 0;
 }
@@ -283,85 +195,84 @@ cv::Mat ImageGrabber::GetImage(const sensor_msgs::ImageConstPtr &img_msg)
 
 void ImageGrabber::SyncWithImu()
 {
-    const double maxTimeDiff = 0.01;
-    while(1)
+  const double maxTimeDiff = 0.01;
+  while(1)
+  {
+    cv::Mat imLeft, imRight;
+    double tImLeft = 0, tImRight = 0;
+    if (!imgLeftBuf.empty()&&!imgRightBuf.empty()&&!mpImuGb->imuBuf.empty())
     {
-        cv::Mat imLeft, imRight;
-        double tImLeft = 0, tImRight = 0;
-        if (!imgLeftBuf.empty()&&!imgRightBuf.empty()&&!mpImuGb->imuBuf.empty())
+      tImLeft = imgLeftBuf.front()->header.stamp.toSec();
+      tImRight = imgRightBuf.front()->header.stamp.toSec();
+
+      this->mBufMutexRight.lock();
+      while((tImLeft-tImRight)>maxTimeDiff && imgRightBuf.size()>1)
+      {
+        imgRightBuf.pop();
+        tImRight = imgRightBuf.front()->header.stamp.toSec();
+      }
+      this->mBufMutexRight.unlock();
+
+      this->mBufMutexLeft.lock();
+      while((tImRight-tImLeft)>maxTimeDiff && imgLeftBuf.size()>1)
+      {
+        imgLeftBuf.pop();
+        tImLeft = imgLeftBuf.front()->header.stamp.toSec();
+      }
+      this->mBufMutexLeft.unlock();
+
+      if((tImLeft-tImRight)>maxTimeDiff || (tImRight-tImLeft)>maxTimeDiff)
+      {
+        // std::cout << "big time difference" << std::endl;
+        continue;
+      }
+      if(tImLeft>mpImuGb->imuBuf.back()->header.stamp.toSec())
+          continue;
+
+      this->mBufMutexLeft.lock();
+      imLeft = GetImage(imgLeftBuf.front());
+      imgLeftBuf.pop();
+      this->mBufMutexLeft.unlock();
+
+      this->mBufMutexRight.lock();
+      imRight = GetImage(imgRightBuf.front());
+      imgRightBuf.pop();
+      this->mBufMutexRight.unlock();
+
+      vector<ORB_SLAM3::IMU::Point> vImuMeas;
+      mpImuGb->mBufMutex.lock();
+      if(!mpImuGb->imuBuf.empty())
+      {
+        // Load imu measurements from buffer
+        vImuMeas.clear();
+        while(!mpImuGb->imuBuf.empty() && mpImuGb->imuBuf.front()->header.stamp.toSec()<=tImLeft)
         {
-            tImLeft = imgLeftBuf.front()->header.stamp.toSec();
-            tImRight = imgRightBuf.front()->header.stamp.toSec();
-
-            this->mBufMutexRight.lock();
-            while((tImLeft-tImRight)>maxTimeDiff && imgRightBuf.size()>1)
-            {
-                imgRightBuf.pop();
-                tImRight = imgRightBuf.front()->header.stamp.toSec();
-            }
-            this->mBufMutexRight.unlock();
-
-            this->mBufMutexLeft.lock();
-            while((tImRight-tImLeft)>maxTimeDiff && imgLeftBuf.size()>1)
-            {
-                imgLeftBuf.pop();
-                tImLeft = imgLeftBuf.front()->header.stamp.toSec();
-            }
-            this->mBufMutexLeft.unlock();
-
-            if((tImLeft-tImRight)>maxTimeDiff || (tImRight-tImLeft)>maxTimeDiff)
-            {
-                continue;
-            }
-            if(tImLeft>mpImuGb->imuBuf.back()->header.stamp.toSec())
-                continue;
-
-            this->mBufMutexLeft.lock();
-            imLeft = GetImage(imgLeftBuf.front());
-            imgLeftBuf.pop();
-            this->mBufMutexLeft.unlock();
-
-            this->mBufMutexRight.lock();
-            imRight = GetImage(imgRightBuf.front());
-            imgRightBuf.pop();
-            this->mBufMutexRight.unlock();
-
-            vector<ORB_SLAM3::IMU::Point> vImuMeas;
-            mpImuGb->mBufMutex.lock();
-            if(!mpImuGb->imuBuf.empty())
-            {
-                vImuMeas.clear();
-                while(!mpImuGb->imuBuf.empty() && mpImuGb->imuBuf.front()->header.stamp.toSec()<=tImLeft)
-                {
-                    double t = mpImuGb->imuBuf.front()->header.stamp.toSec();
-                    cv::Point3f acc(mpImuGb->imuBuf.front()->linear_acceleration.x, mpImuGb->imuBuf.front()->linear_acceleration.y, mpImuGb->imuBuf.front()->linear_acceleration.z);
-                    cv::Point3f gyr(mpImuGb->imuBuf.front()->angular_velocity.x, mpImuGb->imuBuf.front()->angular_velocity.y, mpImuGb->imuBuf.front()->angular_velocity.z);
-                    vImuMeas.push_back(ORB_SLAM3::IMU::Point(acc,gyr,t));
-                    mpImuGb->imuBuf.pop();
-                }
-            }
-            mpImuGb->mBufMutex.unlock();
-            if(mbClahe)
-            {
-                mClahe->apply(imLeft,imLeft);
-                mClahe->apply(imRight,imRight);
-            }
-
-            if(do_rectify)
-            {
-                cv::remap(imLeft,imLeft,M1l,M2l,cv::INTER_LINEAR);
-                cv::remap(imRight,imRight,M1r,M2r,cv::INTER_LINEAR);
-            }
-
-            Sophus::SE3f Tcw = mpSLAM->TrackStereo(imLeft,imRight,tImLeft,vImuMeas);
-
-            // 获取位姿并发布
-            PublishTransform(Tcw, ros::Time(tImLeft));
-
-            std::chrono::milliseconds tSleep(1);
-            std::this_thread::sleep_for(tSleep);
+          double t = mpImuGb->imuBuf.front()->header.stamp.toSec();
+          cv::Point3f acc(mpImuGb->imuBuf.front()->linear_acceleration.x, mpImuGb->imuBuf.front()->linear_acceleration.y, mpImuGb->imuBuf.front()->linear_acceleration.z);
+          cv::Point3f gyr(mpImuGb->imuBuf.front()->angular_velocity.x, mpImuGb->imuBuf.front()->angular_velocity.y, mpImuGb->imuBuf.front()->angular_velocity.z);
+          vImuMeas.push_back(ORB_SLAM3::IMU::Point(acc,gyr,t));
+          mpImuGb->imuBuf.pop();
         }
+      }
+      mpImuGb->mBufMutex.unlock();
+      if(mbClahe)
+      {
+        mClahe->apply(imLeft,imLeft);
+        mClahe->apply(imRight,imRight);
+      }
+
+      if(do_rectify)
+      {
+        cv::remap(imLeft,imLeft,M1l,M2l,cv::INTER_LINEAR);
+        cv::remap(imRight,imRight,M1r,M2r,cv::INTER_LINEAR);
+      }
+
+      mpSLAM->TrackStereo(imLeft,imRight,tImLeft,vImuMeas);
+
+      std::chrono::milliseconds tSleep(1);
+      std::this_thread::sleep_for(tSleep);
     }
+  }
 }
 
 void ImuGrabber::GrabImu(const sensor_msgs::ImuConstPtr &imu_msg)
@@ -373,19 +284,3 @@ void ImuGrabber::GrabImu(const sensor_msgs::ImuConstPtr &imu_msg)
 }
 
 
-void ImageGrabber::PublishTransform(const Sophus::SE3f &Tcw, const ros::Time &stamp)
-{
-    if (!Tcw.translation().isZero() || !Tcw.so3().unit_quaternion().coeffs().isZero()) {
-        tf::Transform transform;
-        tf::Vector3 translation(Tcw.translation()[0], Tcw.translation()[1], Tcw.translation()[2]);
-        tf::Quaternion rotation(Tcw.so3().unit_quaternion().x(), Tcw.so3().unit_quaternion().y(),
-                                Tcw.so3().unit_quaternion().z(), Tcw.so3().unit_quaternion().w());
-
-        transform.setOrigin(translation);
-        transform.setRotation(rotation);
-
-        tfBroadcaster.sendTransform(tf::StampedTransform(transform, stamp, "map", "camera_link"));
-    }
-
-
-}
